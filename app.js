98181ab4ab03585629b295926afb38e1e4bd241c
root <root@project-hornet-app.localdomain>
2017-05-26 15:41:35 +0000

    change app.js to work in the server

--- app.js
+++ app.js
@@ -33,21 +33,23 @@ app.use('/public', express.static(__dirname + '/public'));
 app.use('/api', api);
 app.use(express.static(path.join(__dirname, 'frontend')));
 process.env.PORT = process.env.PORT || 5200;
-
+var alias = 0;
 Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
+  
 
   ifaces[ifname].forEach(function (iface) {
     if ('IPv4' !== iface.family || iface.internal !== false) {
       // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
       return;
     }
-
-    if (alias >= 1) {
+    
+    if (alias == 0) {
       // this single interface has multiple ipv4 addresses
+      ip = iface.address
+      console.log(iface.address);
     } 
     else {
-      ip = iface.address
+      
     }
     ++alias;
   });
